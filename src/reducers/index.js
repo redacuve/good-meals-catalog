b0c95678c6820783a65c50672726cdfccf6f7731
Rey David Cuevas <cuevas.vela@gmail.com>
2020-08-13 13:08:46 -0500

    added GetAreas action, and GetAreasReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import CategoriesListReducer from './CategoriesListReducer';
+import AreasListReducer from './AreasListReducer';
 
 const rootReducer = combineReducers({
   CategoriesList: CategoriesListReducer,
+  AreasList: AreasListReducer,
 });
 
 export default rootReducer;
