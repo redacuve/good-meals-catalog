6fb4bdcd8f7f7cc3ff3acab730cebb60e3c5ebc5
Rey David Cuevas <cuevas.vela@gmail.com>
2020-08-13 12:32:43 -0500

    added hooks in the mealists component

--- src/components/MealsList.js
+++ src/components/MealsList.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import GetCategories from '../actions/GetCategories';
 
 function MealsList() {
-  return (<div>Recipes List</div>);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(GetCategories());
+  }, [dispatch]);
+  const mealCategories = useSelector(state => state.CategoriesList);
+  if (mealCategories.loading === true) {
+    return <div>Loading...</div>;
+  }
+  if (mealCategories.errorMsg !== '') {
+    return (
+      <div>
+        <h2>An error has occured</h2>
+        <p>{mealCategories.errorMsg}</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <ul>
+        {mealCategories.categories.meals.map(meal => (
+          <ul key={meal.strCategory}>
+            <h2>{meal.strCategory}</h2>
+            <Link to={`/category/${meal.strCategory}`}>View Category</Link>
+          </ul>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default MealsList;
