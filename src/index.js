2e6446632059df88810fe64ed6573455c9d5d26c
Rey David Cuevas <cuevas.vela@gmail.com>
2020-08-13 10:49:08 -0500

    added Browser Router and provider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/main.css';
 import './assets/css/index.css';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
