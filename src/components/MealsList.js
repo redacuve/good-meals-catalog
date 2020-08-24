e329ebf42b97f75838812ab560267d5a80a3dde9
Rey David Cuevas <cuevas.vela@gmail.com>
2020-08-24 11:05:18 -0500

    added flags for asssets of the list

--- src/components/MealsList.js
+++ src/components/MealsList.js
@@ -7,14 +7,15 @@ function MealsList(props) {
   return (
     <div>
       <h2>{title}</h2>
-      <ul>
-        {data.map(meal => (
-          <ul key={meal[str]}>
-            <h2>{meal[str]}</h2>
-            <Link to={`/category/${meal[str]}`}>View Category</Link>
-          </ul>
+      <div>
+        {data.map((meal) => (
+          <div key={meal[str]}>
+            <h3>{meal[str]}</h3>
+            <img src={'/assets/img/'+ meal[str] + '.jpg'} alt={meal[str]} />
+            <Link to={`/area/${meal[str]}`}>View Category</Link>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
