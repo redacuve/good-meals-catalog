14802fdb127d917596bdc45d2e3c8659d56ef39f
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-03 12:13:47 -0500

    update navbar style and added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
 import { useHistory } from 'react-router-dom';
+import logo from '../assets/img/goodmeals.png';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   const inputSearch = useRef(null);
@@ -9,10 +11,30 @@ function Navbar() {
     inputSearch.current.value = '';
   };
   return (
-    <nav>
-      <h1>Good Meals Catalog</h1>
-      <input type="search" ref={inputSearch} id="seachbar" />
-      <button type="button" onClick={onSearch}>Search</button>
+    <nav className="w-full">
+      <div className="w-full bg-gray-100 border-b border-grey-light">
+        <div className="w-full container mx-auto flex flex-wrap items-center justify-between py-4">
+          <Link to="/" className="flex items-center">
+            <img src={logo} alt="Good Meals Logo" className="h-12"/>
+            <h1 className="font-bold text-indigo-900 text-base font-extrabold text-xl w-minc ml-1 leading-5">Good Meals Catalog</h1>
+          </Link>
+          <div className="">
+            <input
+              className="shadow appearance-none border rounded py-2 px-3 mr-1 text-gray-700 focus:outline-none focus:shadow-outline"
+              type="search"
+              ref={inputSearch}
+              id="seachbar"
+            />
+            <button
+              className="bg-indigo-400 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full"
+              type="button"
+              onClick={onSearch}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
--- tailwind.js
+++ tailwind.js
@@ -427,6 +427,7 @@ module.exports = {
       '11/12': '91.666667%',
       full: '100%',
       screen: '100vw',
+      minc: 'min-content',
     }),
     zIndex: {
       auto: 'auto',
