0cf1382dbce7ec853dba9b8f8a66a046dc30081e
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-03 10:35:17 -0500

    added function to search a term

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Navbar() {
+  const inputSearch = useRef(null);
+  const { push } = useHistory();
+  const onSearch = () => {
+    push(`/search/${inputSearch.current.value}`);
+    inputSearch.current.value = '';
+  };
   return (
     <nav>
-      <h1>Goog Meals Catalog</h1>
-      <input />
-      <button type="button">Search</button>
+      <h1>Good Meals Catalog</h1>
+      <input type="search" ref={inputSearch} id="seachbar" />
+      <button type="button" onClick={onSearch}>Search</button>
     </nav>
   );
 }
