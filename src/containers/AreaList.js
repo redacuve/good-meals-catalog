3c96f6b70858ef5bf51f42729b64a8d56044b0ea
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-04 14:52:41 -0500

    added style to area list container

--- src/containers/AreaList.js
+++ src/containers/AreaList.js
@@ -24,19 +24,35 @@ function CategoryList(props) {
     return <Error errors={[meals.errorMsg]} />;
   }
   return (
-    <div>
-      Category Search:
-      {term}
-      <div>
-        {
-            meals.meals.map(meal => (
-              <div key={meal.strMeal}>
-                <h3>{meal.strMeal}</h3>
-                <img src={meal.strMealThumb} alt={meal.strMeal} />
-                <Link to={`/meal/${meal.idMeal}`}>View Meal</Link>
+    <div className="p-1">
+      <h2 className="text-orange-900 font-bold text-2xl">
+        Category Search:&nbsp;
+        <span className="font-extrabold underline italic">{term}</span>
+      </h2>
+      <div className="flex flex-wrap items-center">
+        {meals.meals.map(meal => (
+          <Link
+            to={`/meal/${meal.idMeal}`}
+            key={meal.strMeal}
+            className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-1 mb-2 shadow border rounded bg-white mb-5 cursor-default"
+          >
+            <div className="w-full">
+              <img
+                className="w-full"
+                src={meal.strMealThumb}
+                alt={meal.strMeal}
+              />
+            </div>
+            <div className="w-full bg-orange-300">
+              <h3 className="font-bold text-2xl pl-3">{meal.strMeal}</h3>
+            </div>
+            <div className="w-full flex justify-center mt-3">
+              <div className="bg-transparent hover:bg-orange-500 text-orange-700 text-sm font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded cursor-pointer">
+                View Meal
               </div>
-            ))
-          }
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
