7d5453cbc318f44ed216393435830948d7f8ed1c
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-07 11:40:49 -0500

    Created component Index Card to avoid DRY on fullCategories and MealsList containers

--- src/components/FullCategoriesList.js
+++ src/components/FullCategoriesList.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import IndexCard from './IndexCard';
 
 function FullCategoriesList(props) {
   const { list } = props;
@@ -8,35 +8,8 @@ function FullCategoriesList(props) {
     <div>
       <h2 className="text-indigo-900 font-bold text-2xl">Categories</h2>
       <div>
-        {list.map(elem => (
-          <Link
-            className="shadow border rounded bg-white mb-5 cursor-default flex flex-wrap items-center justify-center"
-            key={elem.idCategory}
-            to={`/category/${elem.strCategory}`}
-          >
-            <div className="w-full md:w-1/2 lg:w-2/3 xl:w-1/2 flex mt-1">
-              <img
-                className="w-full"
-                src={elem.strCategoryThumb}
-                alt={elem.strCategory}
-              />
-            </div>
-            <div className="w-full md:w-1/2 lg:w-full xl:w-1/2">
-              <div className="w-full bg-indigo-300">
-                <h3 className="font-bold text-2xl pl-3">{elem.strCategory}</h3>
-              </div>
-              <div className="w-full bg-orange-100">
-                <p className="text-gray-700 text-lg px-3">
-                  {elem.strCategoryDescription}
-                </p>
-              </div>
-              <div className="w-full flex justify-center mt-3 mb-1">
-                <div className="bg-transparent hover:bg-blue-500 text-blue-700 text-sm font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded cursor-pointer">
-                  View Category
-                </div>
-              </div>
-            </div>
-          </Link>
+        {list.map(meal => (
+          <IndexCard key={meal.strCategory} color="blue" hasDescription hasImage linkTo="category" meal={meal} str="strCategory" />
         ))}
       </div>
     </div>
--- src/components/MealsList.js
+++ src/components/MealsList.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import IndexCard from './IndexCard';
 
 function MealsList(props) {
   const { title, data, str } = props;
@@ -9,29 +9,7 @@ function MealsList(props) {
       <h2 className="text-indigo-900 font-bold text-2xl">{title}</h2>
       <div>
         {data.map(meal => (
-          <Link
-            key={meal[str]}
-            to={`/area/${meal[str]}`}
-            className="shadow border rounded bg-white mb-5 cursor-default flex flex-wrap items-center justify-center"
-          >
-            <div className="w-full md:w-1/2 lg:w-2/3 xl:w-1/2 flex">
-              <img
-                className="w-full py-1 md:px-1 lg:px-0 xl:px-1"
-                src={`/assets/img/${meal[str]}.jpg`}
-                alt={meal[str]}
-              />
-            </div>
-            <div className="w-full md:w-1/2 lg:w-full xl:w-1/2">
-              <div className="w-full bg-orange-300 text-center">
-                <h3 className="font-bold text-2xl">{meal[str]}</h3>
-              </div>
-              <div className="w-full flex justify-center mt-3 mb-1">
-                <div className="bg-transparent hover:bg-orange-500 text-orange-700 text-sm font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded cursor-pointer">
-                  View Category
-                </div>
-              </div>
-            </div>
-          </Link>
+          <IndexCard key={meal.strArea} color="orange" hasDescription={false} hasImage={false} linkTo="area" meal={meal} str={str} />
         ))}
       </div>
     </div>
